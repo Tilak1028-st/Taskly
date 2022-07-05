0b9fd912c6dfb918cd13a0ed130719d2a49170d3
Shakya Tilak <75114840+Tilak1028-st@users.noreply.github.com>
2022-07-05 02:49:23 -0700

    Initial Commit

---
+++ Taskly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taskly
+//
+//  Created by PCS213 on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
